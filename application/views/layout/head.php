c4ddcf29664db9af4a8de67d10b084cd7f0bf621
Bruce <skyllerx@gmail.com>
2018-11-05 01:48:33 -0600

    Formulario Encabezado del Dashboard

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -1,10 +1,14 @@
+<!-- Formulario del Dashboard -->
 <!DOCTYPE html>
 <html lang="es-mx">
+<!-- Caracteristicas del Encabezado -->    
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Agregamos el link para Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <!-- Titulo del Encabezado -->
     <title><?= $title ?></title>
 </head>
 <body>

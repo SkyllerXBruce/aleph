8e1884377857628b01c5b59ebe703a53267554bf
Bruce <skyllerx@gmail.com>
2018-11-04 20:06:07 -0600

    Clase Migrate Comentada

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -1,10 +1,14 @@
 <?php
 
+// Clase Migrate para las Migraciones
 class Migrate extends CI_Controller{
 
-        public function index()        {
+        // Este index no carga una vista, si no que sirve para cargar las tablas de la Bases de Datos
+         public function index() {
+                 // Carga la Biblioteca migration
                 $this->load->library('migration');
-                if ($this->migration->current() === FALSE)                {
+                // Si no encuentra la migracion seleccionada manda un Error 
+                if ($this->migration->current() === FALSE) {
                         show_error($this->migration->error_string());
                 }
         }

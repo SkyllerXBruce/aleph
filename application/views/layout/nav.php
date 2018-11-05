ef8970b293d28643c6897b66b282a310d266341d
Bruce <skyllerx@gmail.com>
2018-11-05 03:07:14 -0600

    Formulario de la Barra Superior Comentado

--- application/views/layout/nav.php
+++ application/views/layout/nav.php
@@ -2,7 +2,11 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top" style="background-color: #e3f2fd;">
 	<!-- Titulo -->
 	<span class="navbar-text navbar-brand">Sistema de Encuestas</span>
-	<!-- Agrupamos en el Nav los Elementos y sus Caracteristicas -->
+	<!--  -->
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<!-- Agrupamos en el Navegador los Elementos y sus Caracteristicas -->
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto"></ul>
 		<ul class="navbar-nav">

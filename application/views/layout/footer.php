faa83f4d29783b2a0ffb2d5faf5b5f3554e9f15c
Bruce <skyllerx@gmail.com>
2018-11-05 01:43:59 -0600

    Formulario Footer Comentado

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,4 +1,5 @@
-<!-- FOOTER -->
+<!-- Pie de Pagina del Formulario Dashboard -->
+<!-- Agregamos la Biblioteca de Google JQuery -->
 <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 </body>
 </html>

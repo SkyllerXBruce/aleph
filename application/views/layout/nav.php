d33dcc99e7ffc995a3262a834b7090f6bdc3d8eb
Bruce <skyllerx@gmail.com>
2018-11-05 02:18:33 -0600

    Formulario de Nav Comentado

--- application/views/layout/nav.php
+++ application/views/layout/nav.php
@@ -1,23 +1,23 @@
+<!-- Barra Superior del Dashboard -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top" style="background-color: #e3f2fd;">
-	<span class="navbar-text navbar-brand">
-		Sistema de Encuestas.
-	</span>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
+	<!-- Titulo -->
+	<span class="navbar-text navbar-brand">Sistema de Encuestas</span>
+	<!-- Agrupamos en el Nav los Elementos y sus Caracteristicas -->
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto"></ul>
-           <ul class="navbar-nav">
-           	<li class="nav-item">
-           		<a class="nav-link disabled" href="#"><strong>Rol:</strong> <?= $this->session->rol ?></a>
-           	</li>
-           	<li class="nav-item divider">
-           		<a class="nav-link disabled" href="#"><strong>Nombre de Usuario:</strong> <?= $this->session->nombre_usuario ?></a>
-           	</li>
-           	<li class="nav-item" style="margin-left: 2em;">
-           		<!-- <button class="btn btn-warning" type="submit" id="logout">Cerrar Sesión</button> -->
-           		<a href="<?=base_url('login/logout')?>" class="btn btn-warning">Cerrar Sesión</a>
-           	</li>
-           </ul>
-    </div>
+		<ul class="navbar-nav">
+			<!-- Mostramos el Rol del Usuario -->
+			<li class="nav-item">
+				<a class="nav-link disabled" href=""><strong>Rol de Usuario:</strong> <?= $this->session->rol ?></a>
+			</li>
+			<!-- Mostramos el Nombre del Usuario -->
+			<li class="nav-item divider">
+				<a class="nav-link disabled" href=""><strong>Nombre de Usuario:</strong> <?= $this->session->nombre_usuario ?></a>
+			</li>
+			<!-- Boton Cerrar Sesión -->
+			<li class="nav-item" style="margin-left: 2em;">
+				<a href="<?=base_url('login/logout')?>" class="btn btn-warning">Cerrar Sesión</a>
+			</li>
+		</ul>
+	</div>
 </nav>

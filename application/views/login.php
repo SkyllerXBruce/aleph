591d247cf8f2d5f7d7b4692e99c8e1cbe7a85eb2
Bruce <skyllerx@gmail.com>
2018-11-05 04:05:21 -0600

    Formulario login Comentado

--- application/views/login.php
+++ application/views/login.php
@@ -1,14 +1,18 @@
 <!-- Cuerpo  -->
 <body>
+	<!-- Ajustamos la Posicion de los Elementos en un Contenedor -->
 	<div class="container">
 		<div class="row justify-content-lg-center d-flex align-items-center" style="height: 90vh">
 			<div class="col-lg-6">
+				<!-- Identificamos el Formulario -->
 				<form id="frm-login">
-                    <div class="form-group text-center">
-                        <h1>Login</h1>
-                    </div>
-                    <div class="card">
-                    	<!-- Seccion de Registros y limpieza  -->
+					<!-- Titulo Centrado -->
+					<div class="form-group text-center">
+						<h1>Login</h1>
+					</div>
+					<!-- Creamos una Tarjeta de Presentacion -->
+					<div class="card">
+						<!-- Seccion donde va el Link Limpieza  -->
 						<div class="card-header bg-success mb-3">
 							<ul class="nav nav-pills card-header-pills">
 								<li class="nav-item">
@@ -16,19 +20,23 @@
 								</li>
 							</ul>
 						</div>
-						<!-- Seccion de Etiquetas y Cuadros Input -->
+						<!-- Seccion del Cuerpo de la Tarjeta -->
 						<div class="card-body">
+							<!-- Alerta de Verificacion de Datos -->
 							<div class="form-group" id="alert"></div>
+							<!-- Input de Email -->
 							<div class="form-group" id="email">
 								<label for="exampleInputEmail1">Correo</label>
 								<input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Escriba su email">
 								<div class="invalid-feedback"></div>
 							</div>
+							<!-- Input de Contraseña -->
 							<div class="form-group" id="password">
 								<label for="exampleInputPassword1">Contraseña</label>
 								<input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Ingrese su contraseña">  
 								<div class="invalid-feedback"></div>
 							</div>
+							<!-- Boton Ingresar -->
 							<div class="form-group text-center">
 								<button type="submit" class="btn btn-secondary">Ingresar</button>
 							</div>
@@ -38,7 +46,7 @@
 			</div>
 		</div>
 	</div>
-	<!-- Jquery de la Biblitecas de Google -->
+	<!-- Jquery de la Bibliotecas de Google -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="<?=base_url('assets/js/auth/login.js')?>"></script>
 </body>

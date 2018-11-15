014ef5b1bfb3558449e5bd8bf5a9d394632957a7
Bruce <skyllerx@gmail.com>
2018-11-15 08:30:57 -0600

    Se corrigió el email para poder recibir email por mailtrap

--- application/config/email.php
+++ application/config/email.php
@@ -2,11 +2,11 @@
 
 $config = Array(
   'protocol' => 'smtp',
-	'smtp_host' => 'smtp.mailtrap.io',
+  'smtp_host' => 'smtp.mailtrap.io',
   'smtp_port' => 2525,
   'smtp_user' => 'e9b277850b85ad',
   'smtp_pass' => '6a735dfdd074bb',
   'crlf' => "\r\n",
-	'newline' => "\r\n",
+  'newline' => "\r\n",
 	'mailtype' => "html"
 );

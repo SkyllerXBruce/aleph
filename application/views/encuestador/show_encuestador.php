f2f6025d7bfef9933c4d90e2b04dae2770647663
Bruce <skyllerx@gmail.com>
2018-11-21 03:40:08 -0600

    Se Cambio el Nombre de las vistas enduestador y analista por show_encuestador y show_analista respectivamente

---
+++ application/views/encuestador/show_encuestador.php
@@ -0,0 +1,18 @@
+<!-- Lista de Encuestas -->
+<h3>Lista de Encuestas</h3>
+<!-- Creamos la Tabla -->
+<table class="table">
+	<!-- Titulo de las Columnas con Estilo para la Tabla -->
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Estudios</th>
+      <th scope="col">Cuestionario</th>
+      <th scope="col">Descripción de Cuestionario</th>
+      <th scope="col">Ir a la Encuesta</th>
+    </tr>
+	</thead>
+	<!-- Ciclo para Mostrar Cada uno de los Datos -->
+  <tbody>
+    
+  </tbody>
+</table>

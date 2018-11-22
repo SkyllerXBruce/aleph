2a76a6f105511652ac3228301270a2bec6f62e37
Bruce <skyllerx@gmail.com>
2018-11-22 12:43:23 -0600

    Se creo el formilario inicai encuesta para mostar la encuesta

---
+++ application/views/encuestador/inicia_encuesta.php
@@ -0,0 +1,11 @@
+<!-- Formulario de Datos del Encuestado -->
+<!-- Se agrega el metodo para mandar los datos -->
+<form action="<?= base_url('encuestador') ?>" method="POST">
+	<!-- Titulo de Datos del Encuestado -->
+	<h3>Inicia Encuesta</h3>
+	<hr>
+	<!-- Boton para el Agregar Todos los Datos -->
+  <div class="form-group">
+    <input type="submit" class="btn btn-success" value="Regresar">
+  </div>
+</form>

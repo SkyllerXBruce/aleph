9c708382fc8fe410f0c58b2ac6aa8f9b6c322cd1
Bruce <skyllerx@gmail.com>
2018-11-28 06:29:48 -0600

    Se quitarón metodos no usados del modelo

--- application/models/Encuestados.php
+++ application/models/Encuestados.php
@@ -13,35 +13,15 @@ class Encuestados extends CI_Model{
 	}
 
 	// Métodos para obtener todos los datos de la tabla Cuestionarios
-	public function getQuest(){
-		$sql = $this->db->get('CUESTIONARIOS');
+	public function getStudies(){
+		$sql = $this->db->get('ESTUDIOS');
 		return $sql->result();
-	}
-
-	// Métodos para obtener todos los encuestadores de la tabla usuarios
-	public function buscarStudiesporEncuestador($id){
-		$this->db->where('IdUsuarios',$id);
-		$sql = $this->db->get('ASIGNADOS');
-		return $sql->result();
-	}
-
-	public function buscarQuest($quest){
-		// Buscamos los datos en la base de datos
-		$data = $this->db->get_where('CUESTIONARIOS',array('Nombre_Cuestionario' => $quest));
-		// Si encontramos los datos regresamos un falso 
-        if(!$data->result()){
-            return false;
-        }
-        return $data->row(); // se regresa la tupla de los datos encontrados 
 	}
 
 	public function buscarQuestPorIdEstudio($idestudio){
 		// Buscamos los datos en la base de datos
-		$data = $this->db->get_where('CUESTIONARIOS',array('IdEstudio' => $idestudio));
-		// Si encontramos los datos regresamos un falso 
-        if(!$data->result()){
-            return false;
-        }
+		$this->db->where('IdEstudio',$idestudio);
+		$data = $this->db->get('CUESTIONARIOS');
         return $data->result(); // se regresa la tupla de los datos encontrados 
 	}
 }

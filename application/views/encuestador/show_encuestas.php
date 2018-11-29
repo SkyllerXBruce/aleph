c2abbdc0b272e041fe371e9d1840d19ecee6900b
Bruce <skyllerx@gmail.com>
2018-11-28 20:44:05 -0600

    Se agregó boton para ver los detalles de la encuesta

--- application/views/encuestador/show_encuestas.php
+++ application/views/encuestador/show_encuestas.php
@@ -1,13 +1,15 @@
 <!-- Lista de Encuestas -->
-<h3>Lista de Encuestas</h3>
+<h3>Estudios Asignados</h3>
 <!-- Creamos la Tabla -->
 <table class="table">
 	<!-- Titulo de las Columnas con Estilo para la Tabla -->
   <thead class="thead-dark">
     <tr>
       <th scope="col">Estudios</th>
-      <th scope="col">Cuestionario</th>
-      <th scope="col">Descripción de Cuestionario</th>
+      <th scope="col">Encuestas</th>
+      <th scope="col">Encuestadores</th>
+      <th scope="col">Analistas</th>
+      <th scope="col">Detalles del Estudio</th>
     </tr>
 	</thead>
 	<!-- Ciclo para Mostrar Cada uno de los Datos -->
@@ -16,7 +18,10 @@
     	<tr>
       	<th scope="row"><?= $item['study'] ?></th>
       	<td><?= $item['quest'] ?></td>
-      	<td><?= $item['desc'] ?></td>
+      	<td><?= $item['asignadoe'] ?></td>
+        <td><?= $item['asignadoa'] ?></td>
+        <!-- Botones de Editar y Eliminar -->
+      	<td><a class="btn btn-secondary" href="<?=base_url('encuestador/detalles/'.$item['idquest'])?>" role="button">Detalles de Encuesta</a></td>
     	</tr>
     <?php endforeach; ?>
   </tbody>

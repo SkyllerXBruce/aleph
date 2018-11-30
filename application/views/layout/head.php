dc981737d4ace809f3792ba4e3b979bb50a98713
Bruce <skyllerx@gmail.com>
2018-11-30 16:14:47 -0600

    se Idento el encabezado

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="es-mx">
 <!-- Caracteristicas del Encabezado -->    
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -10,5 +10,5 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <!-- Titulo del Encabezado -->
     <title><?= $title ?></title>
-</head>
+  </head>
 <body>
